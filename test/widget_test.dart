070ae89e892a7804d32a505a329a5ac18076d88d
Snowlly <evanmartin2001@gmail.com>
2025-08-27 10:32:19 +0200

    rename file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_exo3/main.dart';
+import 'package:flutter_exo3/exo4.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
